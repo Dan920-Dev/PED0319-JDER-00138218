97ef1ccc7ec192abb562ce9d53898204d8354ff0
Danii920 <escobare361@gmail.com>
2020-01-14 12:28:43 -0600

    Add CW-05

---
+++ Laboratorio02/CW-05.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Enterprise{
+    string name;
+    int employees;
+};
+
+int main(){
+    Enterprise* array;
+    int size;
+
+    cout<<"Digite cantidad de empresas: "; cin>>size;
+    array= new Enterprise[size];
+    for(int i = 0; i < size; i++){
+        cout<< "Name:\t"; cin>>array[i].name;
+        cin.ignore();
+        cout<< "Empleados:\t"; cin>> (array + i)->employees;
+        cin.ignore();
+        cout<<endl;
+    }
+
+
+    return 0;
+}
